98c941333b46665f5a9e24968e6f44d907212f2e
Yordanos Gebreselasie <yordibege@gmail.com>
2023-10-13 16:19:07 -0600

    Fix Short URL ID display on /urls/:id page

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ const {
   generateRandomString
 } = require('./helpers');
 
-const PORT = 8080; // default port 3000
+const PORT = 8080; 
 const app = express();
 
 app.use(cookieSession({
@@ -59,32 +59,32 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-// GET /urls/:id
 app.get("/urls/:id", (req, res) => {
   const user = users[req.session.userId];
   const url = urlDatabase[req.params.id];
   if (!url) {
     res.status(404).send('<h1>URL not found</h1>');
-  } else if (!user) {
+  } 
+  else if (!user) {
     res.status(401).send('<h1>Please log in or register to view this URL</h1><a href="/login">Login</a><br/><br/><a href="/register">Register</a><br/>');
-  } else if (url.userID !== user.id) {
+  } 
+  else if (url.userID !== user.id) {
     res.status(403).send('<h1>You do not have permission to access this URL</h1>');
-  } else {
+  } 
+  else {
     res.render("urls_show", {user, shortURL: req.params.id, longURL: url.longURL });
   }
 });
 
-app.get("/u/:id", (req, res) => {
-  //const shortURL = req.params.id;
-  //const longURL = urlDatabase[shortURL];
-  const { id } = req.params;
+ app.get("/u/:id", (req, res) => {
+  const id  = req.params.id;
   const longURL = urlDatabase[id].longURL;
  if (longURL) {
   res.redirect(longURL);
     } else {
     res.status(404).send("<h1>Short URL not found.</h1>");
   }
-});
+  });
 
 app.get("/login", (req, res) => {
   const user = users[req.session.userId];
@@ -117,107 +117,89 @@ app.get("/register", (req, res) => {
       longURL,
       userID: user.id
     };
+    console.log(urlDatabase);
    res.redirect(`/urls/${shortURL}`);
-  }
-});
+   }
+  });
 
-app.post("/urls/:id", (req, res) => {
+  app.post("/urls/:id", (req, res) => {
   const user = users[req.session.userId];
   const url = urlDatabase[req.params.id];
- 
   if (!user) {
     res.status(401).send('<h1> You must be logged in to update a URL.</h1> <a herf="/login">Login</a>');
   } else if (!url || url.userID !== user.id) {
     res.status(403).send('<h1> You do not have permission to update this URL.</h1>');
   } else {
     url.longURL = req.body.longURL;
-    res.redirect(`/urls`);
+    res.redirect('/urls');
    }
   });
 
 app.post("/urls/:id/delete", (req, res) => {
   const user = users[req.session.userId];
   const url = urlDatabase[req.params.id];
-
   if (!url) {
     res.status(403).send( '<h1> URL not found </h1>');
   } else if (!user) {
     res.status(403).send('<h1> Please login or register to delete URLs</h1><a href="/login">Login</a><br/><a href="/register">Register</a><br/>');
   } else if (url.userID !== user.id) {
   res.status(403).send('<h1>You do not have permission to delete this URL</h1>');
-  }else {
-    // Delete the URL logic
+  }
+  else {
     delete urlDatabase[req.params.id];
     res.redirect("/urls");
   }
-});
+  });
 
-app.post("/register", (req, res) =>{
+ app.post("/register", (req, res) =>{
   const { email, password } = req.body;
-
-  // Check if email or password are empty
-
-  if (!email || !password) {
+   if (!email || !password) {
     return res.status(400).send('<h3>Email or password cannot be empty.</h3>');
     }
-
-  // Check if the email is already in use
    const foundUser = getUserByEmail(email, users);
   if (foundUser) {
     return res.status(400).send('<h3>Email is already registered.</h3>');
     }
- //hash the password
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  console.log(hashedPassword);
-  
-  // create a new user object with the hashed password
-  const userId = generateRandomString();
-  users[userId] = {
+   const hashedPassword = bcrypt.hashSync(password, 10);
+   console.log(hashedPassword);
+   const userId = generateRandomString();
+   users[userId] = {
     id: userId,
     email,
     password: hashedPassword
   };
-
   req.session.userId = userId;
-// Redirect to the /urls page
   res.redirect("/urls");
-});
+  });
 
-app.post("/login", (req, res) => {
+  app.post("/login", (req, res) => {
   const { email, password } = req.body;
-
-  // Check if email or password are empty
   const foundUser = getUserByEmail(email, users);
   if (!foundUser) {
     return res.status(403).send('<h3> Invalid Email and Password.</h3>');
     }
     console.log(foundUser);
-
-  // Check if the email exists in the users object
-  const user = getUserByEmail(email, users);
-
-  if (!user) {
-   return res.status(403).send('<h3>User with this email does not exist.</h3>');
+    const user = getUserByEmail(email, users);
+    if (!user) {
+    return res.status(403).send('<h3>User with this email does not exist.</h3>');
     }
-  const isPasswordCorrect = bcrypt.compareSync(password, foundUser.password);
-  // Check if the provided password matches the user's password
-  if (!isPasswordCorrect) {
-   //return res.status(403).send("Incorrect password.");
-    
-  }
+   const isPasswordCorrect = bcrypt.compareSync(password, foundUser.password);
+   if (!isPasswordCorrect) {
+   return res.status(403).send("Incorrect password.");
+    }
   req.session.userId = foundUser.id;
   res.redirect("/urls");
-});
+  });
 
-app.post("/logout", (req, res) => {
+  app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/login");
-});
+  });
 
-app.get("/urls.json", (req, res) => {
+  app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
+  });
 
-app.listen(PORT, () => {
+ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
+ });
